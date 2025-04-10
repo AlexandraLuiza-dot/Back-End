41d2da2912b6cb158a5c437a5e4c2fc113e3fd0b
AlexandraLuiza-dot <alexandraluizamg@gmail.com>
2025-04-10 11:40:41 -0400

    first commit

---
+++ atividade01.js
@@ -0,0 +1,20 @@
+//atividade 1
+let nome = "Alexandra";
+let idade = "16";
+
+console.log('Olá meu nome é '+nome+' e eu tenho'+idade+'anos.');
+
+//atividade 2
+let textoMinusculo = texto.toLowerCase();
+let textoMaiusculo = texto.toUpperCase();
+console.log(texto.toLowerCase());
+console.log(texto.toUpperCase());
+
+//atividade 3
+console.log(typeof idade);
+
+//atividade 4
+let peso = 70;
+let altura = 1.75;
+let imc = (peso / (altura*altura)).toFixed(2);
+console.log(`Seu IMC é: $(imc)`);
---
+++ atividade02.js
@@ -0,0 +1,24 @@
+// atividade 1
+let n1 = 10;
+let n2 = 5;
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+
+// atividade 2
+let idade1 = 16;
+let idade2 = 89;
+let comp = idade1 > idade2 ? 'o primeiro é mais velho' : 'o segundo é mais velho';
+console.log(comp);
+
+// atividade 3
+let num = 45;
+let compa = num > 18 ? 'maior de idade' : 'menor de idade'
+console.log(compa);
+
+// atividade 4
+let nume = 18;
+let resto = nume % 2;
+let resul = resto == 0 ? 'o número é par' : 'O número é impar';
+console.log(resul);
