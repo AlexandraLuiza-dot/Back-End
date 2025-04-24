b04623fdab5bd63a57e29cbb0d53231e37b7cb7c
AlexandraLuiza-dot <alexandraluizamg@gmail.com>
2025-04-24 11:46:03 -0400

    Back-End atividades

--- atividade02.js
+++ atividade02.js
@@ -1,23 +1,23 @@
-// atividade 1
-let n1 = 10;
-let n2 = 5;
+// 1
+let n1 = 50;
+let n2 = 25;
 console.log(n1 + n2);
 console.log(n1 - n2);
 console.log(n1 * n2);
 console.log(n1 / n2);
 
-// atividade 2
+// 2
 let idade1 = 16;
-let idade2 = 89;
+let idade2 = 46;
 let comp = idade1 > idade2 ? 'o primeiro é mais velho' : 'o segundo é mais velho';
 console.log(comp);
 
-// atividade 3
+// 3
 let num = 45;
 let compa = num > 18 ? 'maior de idade' : 'menor de idade'
 console.log(compa);
 
-// atividade 4
+// 4
 let nume = 18;
 let resto = nume % 2;
 let resul = resto == 0 ? 'o número é par' : 'O número é impar';
---
+++ atividade03.js
@@ -0,0 +1,17 @@
+// 1
+let apartamento ={
+    quartos : 2,
+    tipo : "Apartamento",
+    endereço : "Avenida Principal, 456 - Centro",
+    andar : 7
+}
+console.log(`${apartamento.tipo} com ${apartamento.quartos} quartos, localizado no ${apartamento.andar}º andar da ${apartamento.endereço}.`);
+
+// 2
+let produtoEmbalado ={
+    nome : "Laptop HP",
+    categoria : "Eletrônicos",
+    peso : 1.5,
+    preco : 3500.00
+}
+console.log(`O produto ${produtoEmbalado.nome}, da categoria ${produtoEmbalado.categoria},pesando ${produtoEmbalado.peso}kg, está a venda por R$ ${produtoEmbalado.preco}`)
